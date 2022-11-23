ce67f91d644b1f078b14fbb83a88f31dfefee259
matheus-dimer <matheuspdimer@gmail.com>
2022-11-23 18:54:56 -0300

    mysql adicionado

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 const port = 3000;
+var mysql = require('mysql');
 var http = require('http');
 var fs = require('fs');
 
