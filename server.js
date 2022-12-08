20af958051264d55a9390944f4ac45e9640442c9
Matheus Dimer <20191GR.TII_I0674@gravatai.ifsul.edu.br>
2022-12-08 10:21:39 -0300

    usuario consegue fazer login

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-// import { query } from 'express-validator';
-
 const url = require('url');
 const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 const port = 3000;
-// const { body, validationResult } = require('express-validator');
-// const { validarEmail } = require('./validar')
 const bodyParser = require('body-parser');
 var mysql = require('mysql');
 var http = require('http');
 var fs = require('fs');
+var session = require('express-session');
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "diehardtcc",
-    port: 3307
+    port: 3306
 
-})
+});
+
+const {
+    sessionName = "SessionLogin"
+} = process.env
+
+const app = express();
+
+app.use(session({
+    name: 'SessionLogin',
+    secret: '2C44-4D44-WppQ38S',
+    resave: false,
+    saveUninitialized: true
+}));
 
 
 con.connect(function (err) {
@@ -28,7 +38,9 @@ con.connect(function (err) {
     con.end
 })
 
-const app = express();
+
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -37,28 +49,68 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    res.render('index.ejs');
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
+    res.render('index.ejs', { logged, idLogged });
 })
 app.get('/ficha', function (req, res) {
-    res.render('sheet.ejs');
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
+    res.render('sheet.ejs', { logged, idLogged });
 })
 
 app.get('/login', function (req, res) {
-    res.render('login.ejs')
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
+    res.render('login.ejs', { logged, idLogged })
+})
+
+app.post('/login', function (req, res) {
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
+
+    var email = req.body['email'];
+    var senha = req.body['senha'];
+
+    var sql = "SELECT * FROM usuario WHERE email = ?"
+    con.query(sql, [email], function (err, result) {
+        if (err) throw err;
+        if (result.length) {
+            console.log('Email confirmado')
+            var sql2 = "SELECT * FROM usuario WHERE senha = ?"
+            con.query(sql2, [senha], function (err, result2) {
+                if (result2.length) {
+                    console.log("Senha confirmada")
+                    req.session.loggedin = true;
+                    req.session.idLogin = result2[0]['nome'];
+                    console.log(req.session.idLogin);
+                    res.redirect('/personagens')
+                    return
+                }
+                else {
+                    res.redirect('/login')
+                    return
+                }
+            })
+        }
+        else {
+            res.redirect('/login')
+            return
+        }
+    })
 })
 
 app.get('/cadastro', function (req, res) {
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
     res.render('cadastro.ejs',
-        { mensagem: "" }
+        { mensagem: "", logged, idLogged }
     )
 })
 
 app.post('/cadastro', function (req, res) {
-
-    // const errors = validarResultado(req)
-    // if (!errors.isEmpty()) {
-    //     return res.redirect('/cadastro'({ errors }))
-    // }
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
     email_exists(req.body['email'], function (exists) {
         var sql = "INSERT INTO usuario (nome, email, senha) VALUES ?"
 
@@ -71,10 +123,10 @@ app.post('/cadastro', function (req, res) {
 
                 console.log("Inserção na tabela:" + result.affectedRows);
             })
-            res.redirect('/')
+            res.redirect('/personagens')
             return
         }
-        res.render('cadastro.ejs', { mensagem: "Este email já está cadastrado!" })
+        res.render('cadastro.ejs', { mensagem: "Este email já está cadastrado!", logged, idLogged })
 
 
     })
@@ -93,13 +145,33 @@ function email_exists(email, callback) {
 }
 
 app.get('/personagens', function (req, res) {
-    res.render('char.ejs')
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
+    /*
+    var sql = 'SELECT * FROM `ficha_jogador` WHERE id_user = ?'
+    
+    con.query(sql, [req.body['id_user']]);
+    */
+    res.render('char.ejs', { logged, idLogged })
 })
 
+
+
 app.get('/perfil', function (req, res) {
-    res.render('perfil.ejs')
+    let logged = req.session.loggedin
+    let idLogged = req.session.idLogin
+    res.render('perfil.ejs', { logged, idLogged })
 })
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+        if (err) throw err
+        res.clearCookie(sessionName)
+        res.redirect('/')
+    })
+
+});
 
 var server = app.listen(port);
 
-console.log("Teste")
+console.log("Servidor Executando")
