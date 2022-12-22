56425e22968f5c1de98cb529d5998f1e4866e97e
Matheus Dimer <20191GR.TII_I0674@gravatai.ifsul.edu.br>
2022-12-22 10:12:58 -0300

    parte de programação pronta, falta agora só landing page

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var mysql = require('mysql');
 var http = require('http');
 var fs = require('fs');
 var session = require('express-session');
+const { check } = require('express-validator');
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -167,12 +168,22 @@ app.get('/personagens', function (req, res) {
         const logged = req.session.loggedin;
         const idLogged = req.session.idLogin;
         let userName = req.session.userName;
+        var checkChars = false
 
 
         const sql = 'SELECT * FROM `ficha_jogador` WHERE id_user = ?'
 
         con.query(sql, [idLogged], (error, data) => {
-            res.render('char.ejs', { logged, idLogged, userName, personagemJogador: data });
+            if (data.length > 0) {
+                checkChars = true;
+                res.render('char.ejs', { logged, idLogged, userName, personagemJogador: data, checkChars });
+            }
+            else {
+                checkChars = false;
+                res.render('char.ejs', { logged, idLogged, userName, personagemJogador: data, checkChars });
+
+            }
+
         });
     }
     else {
@@ -207,13 +218,62 @@ app.get('/perfil', function (req, res) {
         const idLogged = req.session.idLogin;
         let userName = req.session.userName;
 
-        res.render('perfil.ejs', { logged, idLogged, userName })
+        res.render('perfil.ejs', { logged, idLogged, userName, message: '' })
     }
     else {
         res.redirect('/login');
     }
 })
 
+app.post('/change-user/', function (req, res) {
+    if (req.session.loggedin) {
+        const logged = req.session.loggedin;
+        const idLogged = req.session.idLogin;
+        let userName = req.session.userName;
+
+
+        var sql = "UPDATE usuario SET nome = ? WHERE id_user = ?"
+
+        con.query(sql, [req.body.novoUser, idLogged], function (err, result) {
+            if (err) throw err;
+
+            var select = "SELECT * FROM usuario where id_user = ?"
+
+            con.query(select, idLogged, function (err, queryReturn) {
+
+                req.session.userName = queryReturn[0]['nome']
+                res.redirect('/perfil');
+            })
+
+
+
+
+        })
+    }
+})
+
+app.post('/change-pswd', function (req, res) {
+    if (req.session.loggedin) {
+        const logged = req.session.loggedin;
+        const idLogged = req.session.idLogin;
+        let userName = req.session.userName;
+
+        var verSql = "SELECT * FROM usuario WHERE id_user = ?"
+        con.query(verSql, idLogged, function (err, checkReturn) {
+            if (err) throw err;
+            if (checkReturn[0]['senha'] == req.body.senhaVelha) {
+                var updateSql = "UPDATE usuario SET senha = ? where id_user = ?"
+                con.query(updateSql, [req.body.senhaNova, idLogged], function (err, updateResult) {
+                    res.redirect('/perfil')
+                })
+            }
+            else {
+                res.render('perfil.ejs', { logged, idLogged, userName, message: 'Senha atual inválida' })
+            }
+
+        })
+    }
+})
 
 
 app.get('/logout', (req, res) => {
@@ -231,12 +291,12 @@ app.get('/delete/:id', function (req, res) {
     con.query(sql, id, function (err, result) {
         if (err) throw err;
         newSql = "DELETE FROM ficha_jogador WHERE id_ficha = ?"
-        con.query(newSql, id, function(err, newResult){
-          if (err) throw err;
-        console.log("Apagado com sucesso: " + result.affectedRows)
-        res.redirect('/personagens')  
+        con.query(newSql, id, function (err, newResult) {
+            if (err) throw err;
+            console.log("Apagado com sucesso: " + result.affectedRows)
+            res.redirect('/personagens')
         })
-        
+
     })
 })
 
@@ -245,24 +305,94 @@ app.get('/ficha/:id', function (req, res) {
     const idLogged = req.session.idLogin;
     let userName = req.session.userName;
 
-
     var id = req.params.id;
-    var sql = "SELECT * FROM ficha_jogador WHERE id_ficha = ?";
+
+    if (req.session.loggedin) {
+
+        var check = "SELECT * FROM ficha_jogador WHERE id_ficha = ? AND id_user = ?"
+
+        con.query(check, [id, idLogged], function (err, checkResult) {
+            if (err) throw err;
+
+            if (checkResult.length > 0) {
+                var sql = "SELECT * FROM ficha_jogador WHERE id_ficha = ?";
 
 
 
-    con.query(sql, id, function (err, result) {
-        console.log(result)
-        if (err) throw err;
+                con.query(sql, id, function (err, result) {
+                    console.log(result)
+                    if (err) throw err;
 
-        var search = "SELECT * FROM atributos_principais WHERE id_ficha = ?"
+                    var search = "SELECT * FROM atributos_principais WHERE id_ficha = ?"
 
-        con.query(search, id, function (err, newResult) {
-            res.render('sheet.ejs', { logged, idLogged, userName, dadosPlayer: result, dadosPrincipais: newResult })
+                    con.query(search, id, function (err, newResult) {
+                        res.render('sheet.ejs', { logged, idLogged, userName, dadosPlayer: result, dadosPrincipais: newResult })
+                    })
+
+
+                })
+            }
+            else {
+                res.redirect('/personagens')
+            }
+
         })
+    }
+
+})
+
+app.post('/ficha/:id', function (req, res) {
+    const logged = req.session.loggedin;
+    const idLogged = req.session.idLogin;
+    let userName = req.session.userName;
+
+    if (logged) {
+        var id = req.params.id;
+
+        var sql = "UPDATE ficha_jogador SET personagem = ?, raca = ?, classe = ?, alinhamento = ?, antecedente = ?, nivel = ? WHERE id_ficha = ?"
+
+        var values = [
+            [req.body.nomePer],
+            [req.body.raceSel],
+            [req.body.antecedenteSel],
+            [req.body.classSel],
+            [req.body.alignSel],
+            [req.body.levelSel],
+            [id]
+        ]
+
+        con.query(sql, values, function (err, result) {
+            if (err) throw err;
+
+            var queryUpdate = "UPDATE atributos_principais SET forca = ?, destreza = ?, constituicao = ?, inteligencia = ?, sabedoria = ?, carisma = ? , iniciativa = ?, ca = ?, prof = ?, pv_atual = ?, pv_total = ?, percepcao = ? WHERE id_ficha = ?"
+
+            var lastValues = [
+                [req.body.forca],
+                [req.body.destreza],
+                [req.body.constituicao],
+                [req.body.inteligencia],
+                [req.body.sabedoria],
+                [req.body.carisma],
+                [req.body.inic],
+                [req.body.ca],
+                [req.body.prof],
+                [req.body.pva],
+                [req.body.pv],
+                [req.body.sab_p],
+                [id]
+
+            ]
+
+            con.query(queryUpdate, lastValues, function (err, lastResult) {
+                res.redirect('/personagens')
+            })
+        })
+    }
+    else {
+        res.redirect('/login')
+    }
 
 
-    })
 })
 
 var server = app.listen(port);
